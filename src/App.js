3e7a78c365a6d696866b34f06f9a9abdb6829a15
santhoshworks <santhoshp.j4@gmail.com>
2017-05-13 19:56:40 -0400

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Home from './components/Home';
+import Chat from './components/Chat';
+
+import {Platform} from 'react-native';
+
+import {Router, Scene} from 'react-native-router-flux';
+
+class App extends React.Component {
+    render(){
+        return (
+            <Router>
+                <Scene key='root' style={{paddingTop:64}}>
+                    <Scene key='home' component={Home} title='Home' />
+                    <Scene key='Chat' component={Chat} title='Chat' />
+                </Scene>
+            </Router>
+        );
+    }
+}
+
+export default App;
