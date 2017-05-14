62df10af10d54eac8767dbb9a8e6b98afdec1007
santhoshworks <santhoshp.j4@gmail.com>
2017-05-13 21:14:23 -0400

    Created Splash Screen for the App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Home from './components/Home';
 import Chat from './components/Chat';
+import Splash from './components/Splash';
 
 import {Platform} from 'react-native';
 
@@ -10,12 +11,7 @@ import {Router, Scene} from 'react-native-router-flux';
 class App extends React.Component {
     render(){
         return (
-            <Router>
-                <Scene key='root' style={{paddingTop:64}}>
-                    <Scene key='home' component={Home} title='Home' />
-                    <Scene key='Chat' component={Chat} title='Chat' />
-                </Scene>
-            </Router>
+           <Splash />
         );
     }
 }
